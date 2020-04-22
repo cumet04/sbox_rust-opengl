edb5bc1a2529891fdcfd992aa45884cef6eccd30
cumet04 <medalhkr@gmail.com>
2020-04-23 03:28:55 +0900

    texture
    from https://github.com/bwasty/learn-opengl-rs/blob/master/src/_1_getting_started/_4_1_textures.rs

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use self::glfw::{Action, Context, Key};
 extern crate gl;
 use self::gl::types::*;
 
-use std::ffi::CString;
 use std::mem;
 use std::os::raw::c_void;
+use std::path::Path;
 use std::ptr;
 use std::sync::mpsc::Receiver;
+
+extern crate image;
+use image::GenericImageView;
 
 mod shader;
 use shader::Shader;
@@ -50,49 +53,113 @@ fn main() {
     let shader = Shader::new("src/shaders/shader.vs", "src/shaders/shader.fs");
 
     let vao = unsafe {
-        // set up vertex data (and buffer(s)) and configure vertex attributes
-        // ------------------------------------------------------------------
-        // HINT: type annotation is crucial since default for float literals is f64
-        let vertices: [f32; 9] = [
-            -0.5, -0.5, 0.0, // left
-            0.5, -0.5, 0.0, // right
-            0.0, 0.5, 0.0, // top
-        ];
-        let (mut vbo, mut vao) = (0, 0);
+        let mut vao = 0;
         gl::GenVertexArrays(1, &mut vao);
-        gl::GenBuffers(1, &mut vbo);
-        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
         gl::BindVertexArray(vao);
+        vao
+    };
 
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-            &vertices[0] as *const f32 as *const c_void,
-            gl::STATIC_DRAW,
-        );
+    let vbo = {
+        let vertices: [f32; 32] = [
+            // positions, colors, texture coords
+            0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
+            0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
+            -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom left
+            -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // top left
+        ];
 
+        let mut vbo = 0;
+        unsafe {
+            gl::GenBuffers(1, &mut vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                &vertices[0] as *const f32 as *const c_void,
+                gl::STATIC_DRAW,
+            );
+        }
+        vbo
+    };
+
+    let ebo = {
+        let indices = [
+            0, 1, 3, // first Triangle
+            1, 2, 3, // second Triangle
+        ];
+
+        let mut ebo = 0;
+        unsafe {
+            gl::GenBuffers(1, &mut ebo);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+            gl::BufferData(
+                gl::ELEMENT_ARRAY_BUFFER,
+                (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                &indices[0] as *const i32 as *const c_void,
+                gl::STATIC_DRAW,
+            );
+        }
+        ebo
+    };
+
+    unsafe {
+        let stride = 8 * mem::size_of::<GLfloat>() as GLsizei;
+        // position attribute
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+        gl::EnableVertexAttribArray(0);
+        // color attribute
         gl::VertexAttribPointer(
-            0,
+            1,
             3,
             gl::FLOAT,
             gl::FALSE,
-            3 * mem::size_of::<GLfloat>() as GLsizei,
-            ptr::null(),
+            stride,
+            (3 * mem::size_of::<GLfloat>()) as *const c_void,
         );
-        gl::EnableVertexAttribArray(0);
+        gl::EnableVertexAttribArray(1);
+        // texture coord attribute
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            stride,
+            (6 * mem::size_of::<GLfloat>()) as *const c_void,
+        );
+        gl::EnableVertexAttribArray(2);
+    }
 
-        // note that this is allowed, the call to gl::VertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-
-        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-        gl::BindVertexArray(0);
-
-        // uncomment this call to draw in wireframe polygons.
-        // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
-
-        vao
+    // load and create a texture
+    // -------------------------
+    let texture = {
+        let mut texture = 0;
+        // load image, create texture and generate mipmaps
+        let img = image::open(&Path::new("resources/textures/container.jpg"))
+            .expect("Failed to load texture");
+        let data = img.to_bytes();
+        let (width, height) = img.dimensions();
+        unsafe {
+            gl::GenTextures(1, &mut texture);
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+            // set texture filtering parameters
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            gl::TexImage2D(
+                gl::TEXTURE_2D,
+                0,
+                gl::RGB as i32,
+                width as i32,
+                height as i32,
+                0,
+                gl::RGB,
+                gl::UNSIGNED_BYTE,
+                &data[0] as *const u8 as *const c_void,
+            );
+            gl::GenerateMipmap(gl::TEXTURE_2D);
+        }
+        texture
     };
 
     // render loop
@@ -108,25 +175,22 @@ fn main() {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
+            gl::BindTexture(gl::TEXTURE_2D, texture);
             shader.use_program();
-
             gl::BindVertexArray(vao);
-
-            // update shader uniform
-            let time_value = glfw.get_time() as f32;
-            let green_value = time_value.sin() / 2.0 + 0.5;
-            let our_color = CString::new("ourColor").unwrap();
-            let vertex_color_location = gl::GetUniformLocation(shader.id, our_color.as_ptr());
-            gl::Uniform4f(vertex_color_location, 0.0, green_value, 0.0, 1.0);
-
-            // render the triangle
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         window.swap_buffers();
         glfw.poll_events();
+    }
+
+    unsafe {
+        gl::DeleteVertexArrays(1, &vao);
+        gl::DeleteBuffers(1, &vbo);
+        gl::DeleteBuffers(1, &ebo);
     }
 }
 
